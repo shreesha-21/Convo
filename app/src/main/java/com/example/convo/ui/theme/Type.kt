7ab0c05a6386e7cc5d9a76f2f251f4b8da9f1009
shreesha-21 <imsmb21@gmail.com>
2026-01-26 23:37:41 +0530

    Initialization of Git repository

---
+++ app/src/main/java/com/example/convo/ui/theme/Type.kt
@@ -0,0 +1,59 @@
+package com.example.convo.ui.theme
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.convo.R
+
+//  Font family used for the title of the app
+val TitleFontFamily = FontFamily(Font(R.font.varela_round_regular, FontWeight.Normal))
+
+//  Font family used in the app
+val AppFontFamily = FontFamily(
+    Font(R.font.tinos_regular, FontWeight.Normal),
+    Font(R.font.tinos_bold, FontWeight.Bold)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+
+    bodyLarge = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = TitleFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Thin,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+)
+
+
+
