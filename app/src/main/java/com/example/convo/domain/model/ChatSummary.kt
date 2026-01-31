92eea7410bcee209db3c2a2823666033c16ad6e6
shreesha-21 <imsmb21@gmail.com>
2026-02-01 01:38:57 +0530

    Made some changes to the chat summary data class

--- app/src/main/java/com/example/convo/domain/model/ChatSummary.kt
+++ app/src/main/java/com/example/convo/domain/model/ChatSummary.kt
@@ -1,10 +1,10 @@
 package com.example.convo.domain.model
 
 data class ChatSummary(
-    val id: String,
-    val name: String,
-    val lastMessage: String,
-    val time: String,
+    val id: String? = null,
+    val sender: String,
+    val lastMessage: String? = null,
+    val timeStamp: String,
     val unreadCount: Int,
     val avatarUrl: String? = null
 )
